eb086c689d51637b00bb19d1e7c513ccb1a929a8
oficina ido <chacho2k5@gmail.com>
2022-04-22 12:28:34 -0300

    a

---
+++ app/Models/Nivel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Nivel extends Model
+{
+    use HasFactory;
+    // Se cambia el nombre a la tabla cdo difiere del nombre de la migracion
+    // protected $table = 'niveles';
+    // Cuando no queremos trabajar con valores de tipo tiempo
+    // public $timestamps = false;
+
+    // Que campos se van a llenar/utilizar de la tabla (o algo asi)
+    protected $fillable = [
+        'descripcion'
+    ];
+}
