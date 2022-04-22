2264c6dbb34197e9cad54503a72fcde12d9a779f
chacho <chacho2k5@gmail.com>
2022-04-21 21:58:31 -0300

    crud_casa

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NivelController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,12 +18,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])
+    ->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::resource('niveles',NivelController::class);
